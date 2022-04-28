0656ee1f4820c45ad17eefebe8072b2b6475e62d
nbilog <nbilog@ucsd.edu>
2022-04-28 08:29:41 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -13,6 +13,11 @@ public class MarkdownParseTest {
     }
 
     @Test
+    public void subtraction(){
+        assertEquals(4, 2 - 1);
+    }
+
+    @Test
     public void MarkdownParse() throws IOException{
         Path filename = Path.of("test-file.md");
         String content = Files.readString(filename);
