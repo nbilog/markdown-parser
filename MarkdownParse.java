eeb9a88f12067d0cb0a4dab7752f078a2fcdd15f
Nathan M Bilog <cs15lsp22alz@ieng6-203.ucsd.edu>
2022-05-08 18:41:47 -0700

    adding a line

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -31,6 +31,7 @@ public class MarkdownParse {
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
+        
 
         return toReturn;
     }
