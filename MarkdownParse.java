3792731437a2eee0a3aa063c7add22c8b9aefce8
nbilog <nbilog@ucsd.edu>
2022-05-21 23:18:40 -0700

    update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,9 +1,8 @@
-//https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+
 
 public class MarkdownParse {
 
@@ -32,7 +31,6 @@ public class MarkdownParse {
             currentIndex = closeParen + 1;
         }
         
-
         return toReturn;
     }
 
