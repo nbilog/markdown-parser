31e6c1981d894d69604f3e67069adaf89dcd548b
nbilog <nbilog@ucsd.edu>
2022-05-22 16:57:35 -0700

    edit

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,38 +3,35 @@ import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
+import java.util.List;
 
 
 public class MarkdownParseTest {
 
     @Test
     public void MarkdownParse() throws IOException{
-        Path filename = Path.of("snippet1.md");
+        Path filename = Path.of("C:/Users/ThugBlazed/Documents/GitHub/markdown-parser/snippet1.md");
         String content = Files.readString(filename);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals("'google.com", links.get(0));
-        assertEquals("google.com", links.get(1));
-        assertEquals("ucsd.edu", links.get(2));
+        List<String> links = MarkdownParse.getLinks(content);
+        List<String> expect = List.of("'google.com", "google.com", "ucsd.edu");
+        assertEquals(expect, links);
     }
 
     @Test
     public void MarkdownParseTest2() throws IOException{
-        Path filename = Path.of("snippet2.md");
+        Path filename = Path.of("C:/Users/ThugBlazed/Documents/GitHub/markdown-parser/snippet2.md");
         String content = Files.readString(filename);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals("a.com", links.get(0));
-        assertEquals("a.com(())", links.get(1));
-        assertEquals("example.com", links.get(2));
+        List<String> links = MarkdownParse.getLinks(content);
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
+        assertEquals(expect, links);
     }
 
     @Test
     public void MarkdownParseTest3() throws IOException{
-        Path filename = Path.of("snippet3.md");
+        Path filename = Path.of("C:/Users/ThugBlazed/Documents/GitHub/markdown-parser/snippet3.md");
         String content = Files.readString(filename);
-        ArrayList<String> links = MarkdownParse.getLinks(content);
-        assertEquals("https://www.twitter.com", links.get(0));
-        assertEquals("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", links.get(1));
-        assertEquals("https://cse.ucsd.edu/", links.get(2));
+        List<String> links = MarkdownParse.getLinks(content);
+        List<String> expect = List.of("https://www.twitter.com", "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", "https://cse.ucsd.edu/");
+        assertEquals(expect, links);
     }
 }
